50a115dd9366fc9b146fdabff6378b19d74c4aad
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-06-21 21:49:44 +0530

    create a box in TUI

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CScp
+
+A cheating script made using go.
+
+## Get Started
+
+Clone the repository and run :
+
+```
+    go mod download
+```
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/rivo/tview"
+)
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	// fmt.Println("Hello there!")
+	box := tview.NewBox().SetBorder(true).SetTitle("Hello there!")
+	err := tview.NewApplication().SetRoot(box, true).Run()
+	checkNilErr(err)
+}
