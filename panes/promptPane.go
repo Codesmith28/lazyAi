a29f55a7b0346c38e1e4f286732dc73b3159a246
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-06-27 22:56:25 +0530

    update prompt pane to support text

--- panes/promptPane.go
+++ panes/promptPane.go
@@ -1,5 +1,16 @@
 package panes
 
-import "github.com/rivo/tview"
+import (
+	"github.com/rivo/tview"
+)
 
-var PromptPane = tview.NewBox().SetBorder(true).SetTitle(" Prompt ")
+// var PromptPane = tview.NewBox().SetBorder(true).SetTitle(" Prompt ")
+var PromptPane = tview.NewTextArea()
+
+func init() {
+	PromptPane.
+		SetWrap(true).
+		SetBorder(true).
+		SetTitle(" Prompt ").
+		SetBorderPadding(1, 1, 2, 2)
+}
