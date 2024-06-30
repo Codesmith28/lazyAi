23b4b1c47ee0dae1e1c6036229ede00c6be1ed4c
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-06-30 12:33:35 +0530

    add alt prefix to keybindings

--- panes/keybindingsPane.go
+++ panes/keybindingsPane.go
@@ -10,31 +10,33 @@ var KeybindingsPane = tview.NewBox().SetBorder(true).SetTitle(" Keybindings ")
 // SetupGlobalKeybindings sets up global keybindings to focus on each pane.
 func SetupGlobalKeybindings(app *tview.Application) {
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Key() {
-		case tcell.KeyRune:
-			switch event.Rune() {
-			case '1':
-				app.SetFocus(InputPane)
-				return nil
-			case '2':
-				app.SetFocus(ModelsPane)
-				return nil
-			case '3':
-				app.SetFocus(OutputPane)
-				return nil
-			case '4':
-				app.SetFocus(HistoryPane)
-				return nil
-			case '5':
-				app.SetFocus(PromptPane)
-				return nil
-			case '?':
-				app.SetFocus(KeybindingsPane)
-				return nil
-			case 's':
-				saveCurrentState()
-			case 'o':
-				createNewState()
+		if event.Modifiers()&tcell.ModAlt != 0 {
+			switch event.Key() {
+			case tcell.KeyRune:
+				switch event.Rune() {
+				case '1':
+					app.SetFocus(InputPane)
+					return nil
+				case '2':
+					app.SetFocus(ModelsPane)
+					return nil
+				case '3':
+					app.SetFocus(OutputPane)
+					return nil
+				case '4':
+					app.SetFocus(HistoryPane)
+					return nil
+				case '5':
+					app.SetFocus(PromptPane)
+					return nil
+				case '?':
+					app.SetFocus(KeybindingsPane)
+					return nil
+				case 's':
+					saveCurrentState()
+				case 'o':
+					createNewState()
+				}
 			}
 		}
 		return event
