c8e142a716ae9122d0465abef03442e80caf3e40
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-07-10 12:38:09 +0530

    refactor panes

--- main.go
+++ main.go
@@ -13,19 +13,6 @@ import (
 )
 
 var (
-	HistoryPane     = panes.HistoryPane
-	ModelsPane      = panes.ModelsPane
-	OutputPane      = panes.OutputPane
-	PromptPane      = panes.PromptPane
-	KeybindingsPane = panes.KeybindingsPane
-	InputPane       = panes.InputPane
-
-	ModelList  = panes.ModelList
-	PromptText = panes.PromptText
-	InputText  = panes.InputText
-	OutputText = panes.OutputText
-	Selected   = panes.Selected
-
 	FileLocation    string
 	HistoryLocation string
 )
@@ -56,7 +43,7 @@ func main() {
 			checkNilErr(err)
 
 			log.Println("Starting clipboard monitoring after credential input.")
-			setupMainUI(app)
+			panes.SetupMainUI(app, HistoryLocation)
 		})
 
 		app.SetRoot(credentialModal, true)
@@ -65,26 +52,6 @@ func main() {
 		checkNilErr(err)
 	} else {
 		log.Println("Starting clipboard monitoring with existing credentials.")
-		setupMainUI(app)
+		panes.SetupMainUI(app, HistoryLocation)
 	}
-}
-
-func setupMainUI(app *tview.Application) {
-	group2 := panes.CreateGroup2(HistoryPane, ModelsPane)
-	group4 := panes.CreateGroup4(InputPane, PromptPane)
-	group3 := panes.CreateGroup3(group4, OutputPane)
-	group1 := panes.CreateGroup1(group2, group3)
-	mainFlex := panes.CreateMainFlex(group1, KeybindingsPane)
-
-	panes.SetupGlobalKeybindings(app, HistoryLocation)
-	panes.InitHistoryPane(HistoryLocation)
-
-	app.SetRoot(mainFlex, true)
-	log.Println("Running app for main UI.")
-
-	panes.StartClipboardMonitoring(app)
-	panes.ApplySystemNavConfig(app)
-
-	err := app.Run()
-	checkNilErr(err)
 }
--- panes/panes.go
+++ panes/panes.go
@@ -1,6 +1,30 @@
 package panes
 
-import "log"
+import (
+	"log"
+
+	"github.com/rivo/tview"
+)
+
+func SetupMainUI(app *tview.Application, HistoryLocation string) {
+	group2 := CreateGroup2(HistoryPane, ModelsPane)
+	group4 := CreateGroup4(InputPane, PromptPane)
+	group3 := CreateGroup3(group4, OutputPane)
+	group1 := CreateGroup1(group2, group3)
+	mainFlex := CreateMainFlex(group1, KeybindingsPane)
+
+	SetupGlobalKeybindings(app, HistoryLocation)
+	InitHistoryPane(HistoryLocation)
+
+	app.SetRoot(mainFlex, true)
+	log.Println("Running app for main UI.")
+
+	StartClipboardMonitoring(app)
+	ApplySystemNavConfig(app)
+
+	err := app.Run()
+	checkNilErr(err)
+}
 
 func UpdateInputPane() {
 	InputPane.SetText(InputText.InputString)
