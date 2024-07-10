36d9df69be6fb0e38e2a342dac8609e3a54e60bd
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-07-10 18:05:14 +0530

    refactor setup mainUI

--- panes/main.go
+++ panes/main.go
@@ -2,28 +2,12 @@ package panes
 
 import (
 	"log"
-
-	"github.com/rivo/tview"
 )
 
-func SetupMainUI(app *tview.Application, HistoryLocation string) {
-	group2 := CreateGroup2(HistoryPane, ModelsPane)
-	group4 := CreateGroup4(InputPane, PromptPane)
-	group3 := CreateGroup3(group4, OutputPane)
-	group1 := CreateGroup1(group2, group3)
-	mainFlex := CreateMainFlex(group1, KeybindingsPane)
-
-	SetupGlobalKeybindings(app, HistoryLocation)
-	InitHistoryPane(HistoryLocation)
-
-	app.SetRoot(mainFlex, true)
-	log.Println("Running app for main UI.")
-
-	StartClipboardMonitoring(app)
-	ApplySystemNavConfig(app)
-
-	err := app.Run()
-	checkNilErr(err)
+func checkNilErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func UpdateInputPane() {
@@ -40,10 +24,4 @@ func UpdatePromptPane() {
 
 func SelectModel(model string) {
 	Selected.SelectedModel = model
-}
-
-func checkNilErr(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
 }
